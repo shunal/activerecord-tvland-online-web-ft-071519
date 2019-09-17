b3767edb0745a643ac82f627e54534515b06c88c
Steven Hunal <stevehunal@gmail.com>
2019-09-17 03:49:22 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
+require 'pry'
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def actors_list
+    self.actors.collect do |t|
+      t.full_name
+    end 
+  end
 end
